3359e3b3e6e132a83bf78d7664cab0ec8b917087
Piotr <piotrcelej@interia.pl>
2025-12-30 09:38:16 +0100

    przygotowane tablice z pomieszczeniami

--- elements.py
+++ elements.py
@@ -1,4 +1,6 @@
 #elementy losowe, jakie występują w FATD na podstawie rzutów kośćmi
+
+#TODO: rozbudować o inne elementy losowe, dodać wszystkie tabele z FATD na podstawie książki
 import random
 def roll_dice(num_dice, num_sides):
     return sum(random.randint(1, num_sides) for _ in range(num_dice))
--- tiles.py
+++ tiles.py
@@ -1,7 +1,21 @@
-# Tiles of the dungeon repository and methods
+# plany podziemi, jakie występują w FATD
 
+#korytarze
 tiles = [
-    "XX XX\nXX XX\nX  XX\nX XXX\n"
+    "## XX\nXX XX\nX  XX\nX XXX\n",
 ]
 
 print (tiles[0])
+
+#pomieszczenia
+
+rooms = [
+    "XXXXXX\nX    X\nX XX X\nX    X\nXXXXXX\n",
+]
+print (rooms[0])
+
+#wejścia
+entrances = [
+    "  X  \n XXX \nXXXXX\n",
+]
+print (entrances[0])
